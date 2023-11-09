90d835e395853fe57ddef162c2344344b342aed6
eli barak <barak3656@gmail.com>
2023-11-09 14:34:46 +0200

    srever starting

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const env = require('dotenv').config()
+
+const User = require('./db');
+const routes = require('./router');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+
+app.use(cors())
+app.use(bodyParser.json());
+
+
+
+
+routes(app, User, jwt, bcrypt);
+
+function verifyToken(req, res, next) {
+    const token = req.headers.authorization;
+    if (!token) return res.status(403).send({ auth: false, message: 'No token provided.' });
+  
+    jwt.verify(token, 'secret', (err, decoded) => {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+  
+      req.userId = decoded.id;
+      next();
+    });
+  }
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello Infra-jerusalem-2 Team! This is our Express server.');
 });
 
-// MongoDB connection setup
-// mongoose.connect('mongodb://localhost/database-name', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }).then(() => {
-//     console.log('Connected to MongoDB');
-// }).catch((err) => {
-//     console.error('Connection error', err);
-// });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ router.js
@@ -0,0 +1,99 @@
+const { validationResult, body } = require("express-validator");
+
+function routes(app, User, jwt, bcrypt) {
+  const signupValidation = [
+    body("firstName").notEmpty().withMessage("First name is required"),
+    body("lastName").notEmpty().withMessage("Last name is required"),
+    body("email").isEmail().withMessage("Invalid email address"),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters long"),
+  ];
+
+  app.post("/signup", signupValidation, async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { firstName, lastName, password, email } = req.body;
+      console.log(req.body)
+
+      const userMail = await User.findOne({ email: email });
+      if (userMail) {
+        console.log('he email already exists')
+        return res
+          .status(500)
+          
+          .send({ auth: false, message: "The email already exists" });
+      }
+
+      const hashedPassword = bcrypt.hashSync(password, 8);
+
+      const user = new User({
+        firstName,
+        lastName,
+        password: hashedPassword,
+        email,
+      });
+
+      await user.save();
+
+ 
+
+      res.status(200).send({ auth: true });
+    } catch (error) {
+      console.error(error);
+
+      res.status(500).send({ auth: false, message: "User registration failed." });
+    }
+  });
+
+  const signinValidation = [
+    body("email").isEmail().withMessage("Invalid email address"),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters long"),
+  ];
+
+  app.post("/signin", signinValidation, async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { email, password } = req.body;
+  
+
+      const user = await User.findOne({ email: email });
+      console.log(user)
+
+      if (!user) {
+        return res.status(404).send({ auth: false, message: "User not found" });
+      }
+      const hashedPassword = bcrypt.hashSync(password, 8);
+      console.log(hashedPassword)
+      const isPasswordValid = hashedPassword && user.password;
+      console.log(isPasswordValid)
+
+
+      if (!isPasswordValid) {
+        return res.status(401).send({ auth: false, message: "Invalid password" });
+      }
+
+      const token = jwt.sign(
+        { id: user._id, name: user.firstName + " " + user.lastName },
+        "secret",
+        { expiresIn: 100000 }
+      );
+
+      res.status(200).send({ auth: true, token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ auth: false, message: "Sign-in failed." });
+    }
+  });
+}
+
+module.exports = routes;
